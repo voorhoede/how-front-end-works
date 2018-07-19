35479268279ae5e79bf01890c203d5159340c313
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2018-07-19 15:37:06 +0200

    feat(label): add hover animation to label

--- src/components/label.vue
+++ src/components/label.vue
@@ -1,7 +1,9 @@
 <template>
-  <a class="label" :href="labelInfo.slug" :style="`background-color: ${labelInfo.color};`">
-      <ConceptIcon v-if="labelInfo.icon"  :backgroundColor="labelInfo.color" :imageUrl="labelInfo.icon" />
+  <a class="label" :href="labelInfo.slug" :style="`background: ${labelInfo.color};`">
+    <div class="label__test">
+      <ConceptIcon v-if="labelInfo.icon" :imageUrl="labelInfo.icon" />
       {{ labelInfo.name }}
+    </div>
   </a>
 </template>
 
@@ -18,11 +20,27 @@ export default {
 @import '~assets/core.css';
 
 .label {
+  display: inline-block;
+  border-radius: 30px;
+}
+
+.label__test {
   display: flex;
   align-items: center;
   padding: .3rem .9rem;
   border-radius: 30px;
-  background: none;
+  background-color: rgba(255, 255, 255, 0);
+  transition: background-color .08s linear;
+}
+
+.label:focus {
+  outline: none;
+}
+
+.label:hover .label__test,
+.label:focus .label__test {
+  background-color: rgba(255, 255, 255, .5);
+  transition: background-color .15s linear;
 }
 
 .label .concept-icon {
