52fe7308f72b1caa3270d1ba917851ae9c3e4560
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2018-08-01 13:49:58 +0200

    feat(navigation): implement new design of navigation

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -33,28 +33,36 @@ export default {
     margin-bottom: calc(var(--spacing-default) * 3);
     padding-bottom: var(--spacing-default);
   }
+}
 
-  .navigation ul {
-    display: flex;
-    flex-wrap: wrap;
-  }
+.navigation ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 
 .navigation__item {
+  flex: 0 0 40%;
   margin-bottom: var(--spacing-default);
+}
+
+@media (min-width: 400px) {
+  .navigation__item {
+    flex: 0 0 25%;
+  }
 }
 
 @media (min-width: 600px) {
   .navigation__item {
-    flex: 0 0 50%;
-    padding-right: var(--spacing-default);
+    flex: 0 0 20%;
   }
 }
 
 .navigation__item a {
-  display: flex;
-  align-items: center;
+  display: block;
   background: none;
+  font-size: var(--font-size-smaller);
+  text-align: center;
   line-height: 1.4;
   border: none;
 }
@@ -71,7 +79,7 @@ export default {
 
 .navigation .concept-icon {
   flex: 0 0 auto;
-  margin-right: var(--spacing-default);
+  margin: 0 auto var(--spacing-half) auto;
   width: 3rem;
   height: 3rem;
 }
