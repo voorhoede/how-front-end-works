0b1f4eba3ae070833f500b9021813ce0771b8992
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2018-08-23 15:05:17 +0200

    fix(seo): make og properties explicit

--- src/lib/seo-head.js
+++ src/lib/seo-head.js
@@ -4,13 +4,13 @@ export default function (seo) {
     title: seo.title,
     meta: [
       { hid: 'description', name: 'description', content: seo.description },
-      { hid: 'og:title', name: 'og:title', content: seo.title },
-      { hid: 'og:description', name: 'og:description', content: seo.description },
+      { hid: 'og:title', property: 'og:title', content: seo.title },
+      { hid: 'og:description', property: 'og:description', content: seo.description },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
       { hid: 'twitter:site', name: 'twitter:site', content: '@devoorhoede' },
-      image.url && { hid: 'og:image', name: 'og:image', content: `${image.url}?auto=format&fm=jpg&auto=quality` },
-      image.width && { hid: 'og:image:width', name: 'og:image:width', content: image.width },
-      image.height && { hid: 'og:image:height', name: 'og:image:height', content: image.height },
+      image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}?auto=format&fm=jpg&auto=quality` },
+      image.width && { hid: 'og:image:width', property: 'og:image:width', content: image.width },
+      image.height && { hid: 'og:image:height', property: 'og:image:height', content: image.height },
     ].filter(Boolean)
   }
 }
