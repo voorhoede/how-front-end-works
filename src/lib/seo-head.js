1f4a6f11f0baea65afd8da8362af64283429aa4d
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2018-08-23 15:36:03 +0200

    refactor(seo): replace og:url with canonical url

--- src/lib/seo-head.js
+++ src/lib/seo-head.js
@@ -6,12 +6,14 @@ export default function (seo) {
       { hid: 'description', name: 'description', content: seo.description },
       { hid: 'og:title', property: 'og:title', content: seo.title },
       { hid: 'og:description', property: 'og:description', content: seo.description },
-      { hid: 'og:url', property: 'og:url', content: `https://how-front-end-works.com/${seo.url}` },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
       { hid: 'twitter:site', name: 'twitter:site', content: '@devoorhoede' },
       image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}?auto=format&fm=jpg&auto=quality` },
       image.width && { hid: 'og:image:width', property: 'og:image:width', content: image.width },
       image.height && { hid: 'og:image:height', property: 'og:image:height', content: image.height },
-    ].filter(Boolean)
+    ].filter(Boolean),
+    link: [
+      { rel: 'canonical', href: `https://how-front-end-works.com/${seo.url}` },
+    ]
   }
 }
