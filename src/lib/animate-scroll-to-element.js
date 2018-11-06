3e3e6a7a2246e0726c289a111a94e055d980b58d
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2018-11-06 13:28:25 +0100

    feat(animate-scroll-to-element): improve animation

--- src/lib/animate-scroll-to-element.js
+++ src/lib/animate-scroll-to-element.js
@@ -1,4 +1,34 @@
 export default function animateScrollToElement(target) {
-  const targetElement = document.querySelector(target)
-  targetElement.scrollIntoView({behavior: 'smooth', block: 'start'})
+  const reducedMotionMediaQuery = window.matchMedia('(prefers-reduced-motion)')
+
+  if(!reducedMotionMediaQuery.matches) {
+    const targetElement = document.querySelector(target)
+    const endPosition = targetElement.getBoundingClientRect().top
+    const startPosition = window.scrollY
+    const scrollDuration = 700
+    let startTimestamp = null
+
+    location.hash = target
+    window.requestAnimationFrame(step)
+
+    function step(timestamp) {
+      if(!startTimestamp) {
+        startTimestamp = timestamp
+      }
+
+      const animationProgress = timestamp - startTimestamp
+
+      const currentPosition = easeOutQuad(animationProgress, startPosition, endPosition, scrollDuration)
+
+      window.scrollTo(0, currentPosition)
+
+      if(animationProgress < scrollDuration) {
+        window.requestAnimationFrame(step)
+      }
+    }
+
+    function easeOutQuad(currentTime, startValue, scrollDistanceValue, duration) {
+      return -scrollDistanceValue * (currentTime /= duration) * (currentTime - 2) + startValue
+    }
+  }
 }
