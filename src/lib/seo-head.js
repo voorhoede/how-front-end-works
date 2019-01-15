eb8b797d6b0a37db77f01c562e629ab7d83f799b
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2019-01-15 14:25:21 +0100

    fix(seo): improve facebook og info

--- src/lib/seo-head.js
+++ src/lib/seo-head.js
@@ -4,13 +4,15 @@ export default function (seo) {
     title: seo.title,
     meta: [
       { hid: 'description', name: 'description', content: seo.description },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: `https://how-front-end-works.com/${seo.url}` },
       { hid: 'og:title', property: 'og:title', content: seo.title },
       { hid: 'og:description', property: 'og:description', content: seo.description },
-      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
-      { hid: 'twitter:site', name: 'twitter:site', content: '@devoorhoede' },
       image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}?auto=format&fm=jpg&auto=quality` },
       image.width && { hid: 'og:image:width', property: 'og:image:width', content: image.width },
       image.height && { hid: 'og:image:height', property: 'og:image:height', content: image.height },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@devoorhoede' },
     ].filter(Boolean),
     link: [
       { rel: 'canonical', href: `https://how-front-end-works.com/${seo.url}` },
