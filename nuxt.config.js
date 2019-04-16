e2eb33b735766c5c0cc57d5b2ea11908ca73d2f3
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2019-04-16 16:04:19 +0200

    feat(app): add ios splash screens

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,13 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-1125x2436.png', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'}, // iPhone X
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-750x1334.png', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'}, // iPhone 8, 7, 6s, 6
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-1242x2208.png', media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)'}, // iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-640x1136.png', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'}, // iPhone 5
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-1536x2048.png', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'}, // iPad Mini, Air
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-1668x2224.png', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'}, // iPad Pro 10.5"
+      { rel: 'apple-touch-startup-image', href: '/pwa/ios-launch-2048x2732.png', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'}, // iPad Pro 12.9"
     ]
   },
 
@@ -52,7 +59,7 @@ module.exports = {
         background_color: '#c5f3f0',
       },
       icon: {
-        iconSrc: 'src/static/icons/icon.png',
+        iconSrc: 'src/static/pwa/icon.png',
       },
     }],
     ['@nuxtjs/sitemap', { // https://github.com/nuxt-community/sitemap-module
