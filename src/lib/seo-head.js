463c2d302a102378179d784979181523b0582b77
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2019-04-24 13:28:18 +0200

    fix(seo): fix og data

--- src/lib/seo-head.js
+++ src/lib/seo-head.js
@@ -5,10 +5,10 @@ export default function (seo) {
     meta: [
       { hid: 'description', name: 'description', content: seo.description },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: `https://how-front-end-works.com/${seo.url}` },
+      { hid: 'og:url', property: 'og:url', content: `https://howfrontendworks.com/${seo.url}` },
       { hid: 'og:title', property: 'og:title', content: seo.title },
       { hid: 'og:description', property: 'og:description', content: seo.description },
-      image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}?auto=format&fm=jpg&auto=quality` },
+      image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}` },
       image.width && { hid: 'og:image:width', property: 'og:image:width', content: image.width },
       image.height && { hid: 'og:image:height', property: 'og:image:height', content: image.height },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
