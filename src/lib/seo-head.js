b7c9d00c94c31cb1e67bea25bd6c14b4ad86b4ab
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2019-04-24 13:36:26 +0200

    Fix SEO (#34)

--- src/lib/seo-head.js
+++ src/lib/seo-head.js
@@ -5,17 +5,17 @@ export default function (seo) {
     meta: [
       { hid: 'description', name: 'description', content: seo.description },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: `https://how-front-end-works.com/${seo.url}` },
+      { hid: 'og:url', property: 'og:url', content: `https://www.howfrontendworks.com/${seo.url}` },
       { hid: 'og:title', property: 'og:title', content: seo.title },
       { hid: 'og:description', property: 'og:description', content: seo.description },
-      image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}?auto=format&fm=jpg&auto=quality` },
+      image.url && { hid: 'og:image', property: 'og:image', content: `${image.url}` },
       image.width && { hid: 'og:image:width', property: 'og:image:width', content: image.width },
       image.height && { hid: 'og:image:height', property: 'og:image:height', content: image.height },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
       { hid: 'twitter:site', name: 'twitter:site', content: '@devoorhoede' },
     ].filter(Boolean),
     link: [
-      { rel: 'canonical', href: `https://how-front-end-works.com/${seo.url}` },
+      { rel: 'canonical', href: `https://www.howfrontendworks.com/${seo.url}` },
     ]
   }
 }
