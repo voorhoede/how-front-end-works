e59c59207fc1aa4166d108c3013e58f268c3881d
Anne Fortuin <phortuin@gmail.com>
2020-01-13 12:15:16 +0100

    refactor: use promises to write paper docs, replace isomorphic fetch with node fetch (as script runs during build only)

--- src/lib/generate-papers.js
+++ src/lib/generate-papers.js
@@ -1,30 +1,30 @@
-const { mkdir, writeFileSync } = require('fs')
+const promisify = require('util').promisify
+const mkdir = promisify(require('mkdirp'))
+const writeFile = promisify(require('fs').writeFile)
 const dotenv = require('dotenv-safe').config()
 const Dropbox = require('dropbox').Dropbox
-const fetch = require('isomorphic-fetch')
+const fetch = require('node-fetch')
 
-const dataDir = `src/static/data/papers/`
+const DATA_DIRECTORY = `./src/static/data/papers/`
 
 const removeTitle = doc => doc.split('\n').slice(1).join('\n').trim()
 
-const savePaperLocally = (doc, ID) => {
-  mkdir(dataDir, () => {
-    writeFileSync(
-      `${dataDir}${ID}.md`,
-      removeTitle(doc.fileBinary.toString('utf8')),
-      'utf8'
-    )
-  })
+const savePaperLocally = (doc, id) => {
+  return mkdir(DATA_DIRECTORY)
+    .then(() => writeFile(`${DATA_DIRECTORY}${id}.md`, removeTitle(doc.fileBinary.toString('utf8')), 'utf8'))
 }
 
-module.exports = ID => {
-  if (ID === null) return
-  const dbx = new Dropbox({
+module.exports = id => {
+  if (!id) {
+    return
+  }
+
+  const dropbox = new Dropbox({
     accessToken: process.env.DROPBOX_ACCESS_TOKEN,
     fetch
   })
 
-  return dbx
-    .paperDocsDownload({ doc_id: ID, export_format: 'markdown' })
-    .then(response => savePaperLocally(response, ID))
+  return dropbox
+    .paperDocsDownload({ doc_id: id, export_format: 'markdown' })
+    .then(response => savePaperLocally(response, id))
 }
